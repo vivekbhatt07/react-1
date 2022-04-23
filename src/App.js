a11eb664ba13459775fa540f45c5ea44eecf9ffb
vivekbhatt07 <vivekbhatt618@gmail.com>
2022-04-23 17:18:14 +0530

    Main Styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
-
+import Main from "./Components/Main";
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Main />
     </div>
   );
 }
---
+++ src/Components/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <main className="main">
+      <h1 className="main__head">Fun facts about React</h1>
+      <ul className="main__list">
+        <li className="main__item">Was first released in 2013</li>
+        <li className="main__item">Was originally created by Jordan Walke</li>
+        <li className="main__item">Has well over 100K stars on GitHub</li>
+        <li className="main__item">Is maintained by Facebook</li>
+        <li className="main__item">
+          Powers thousands of enterprise apps, including mobile apps
+        </li>
+      </ul>
+    </main>
+  );
+}
